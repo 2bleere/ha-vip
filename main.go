f0e854f3c3839ebc7b1b7f23a3edd48d82f49e15
2bleere <ramonluis@gmail.com>
2025-06-15 21:42:37 -0500

    Add version information display

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+    "flag"
+    "fmt"
     "log"
     "os"
     "os/signal"
     "syscall"
 )
 
+// These values are set at build time using -ldflags
+var (
+    version = "dev"
+    commit  = "none"
+    date    = "unknown"
+)
+
 func main() {
-    cfg := LoadConfig("config.yaml")
-    log.Println("Starting HA VIP Manager for", cfg.NodeID)
+    // Parse command-line flags
+    configFile := flag.String("config", "config.yaml", "Path to configuration file")
+    showVersion := flag.Bool("version", false, "Show version information and exit")
+    flag.Parse()
+    
+    // Show version if requested
+    if *showVersion {
+        fmt.Printf("HA VIP Manager v%s (commit: %s, built: %s)\n", version, commit, date)
+        os.Exit(0)
+    }
+    
+    cfg := LoadConfig(*configFile)
+    log.Printf("Starting HA VIP Manager v%s for %s", version, cfg.NodeID)
 
     hb := NewHeartbeat(cfg)
     go hb.Start()
